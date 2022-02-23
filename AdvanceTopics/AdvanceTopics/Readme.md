669aa61450b126c1e36355b2ba54408fadba6ef2
mindball <mincho.balaliew@gmail.com>
2022-02-23 09:44:59 +0200

    Start tutorial

---
+++ AdvanceTopics/AdvanceTopics/Readme.md
@@ -0,0 +1,29 @@
+﻿# Generics
+
+## Generic methods
+### Use cases
+```
+Use generics to build reausable, type neutral methods
+```
+```
+Use 'T' as the type parameter for methods with one type parameter
+```
+```c#
+public TReturn RetrieveValue<TReturn, TParameter>(string sql, TParameter sqlParameter)
+{
+	....
+}
+```
+
+### Not use cases
+```
+Using generics when not need can cause confusion and
+hard to find bugs
+```
+```
+Using single letter names when defining multiple type parameters is 
+better way to use descriptive name. Example: T -> TResult or TMessage 
+```
+
+## Constraints
+![constraints](constraints.JPG "constraints").
---
+++ AdvanceTopics/AdvanceTopics/TGeneric.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvanceTopics
+{
+    public class TWithoutGeneric
+    {
+        //Кога имплементираме метод и класа не е generic трябва да упоменем <T>
+        public virtual TReturn TRetrieveValue<TReturn>(string sql, TReturn defaultValue)
+        {
+            var valueType = defaultValue;
+            return valueType;
+        }
+    }
+
+    public class TGeneric<T> : TWithoutGeneric
+    {
+        //generic class
+        public T RetrieveValue(string sql, T defaultValue)
+        {
+            T valueType = defaultValue;
+            return valueType;
+        }
+
+#pragma warning disable CS0693 // Type parameter has the same name as the type parameter from outer type
+        public override T TRetrieveValue<T>(string sql, T defaultValue)
+#pragma warning restore CS0693 // Type parameter has the same name as the type parameter from outer type
+        {
+            return base.TRetrieveValue(sql, defaultValue);
+        }
+    }
+}
