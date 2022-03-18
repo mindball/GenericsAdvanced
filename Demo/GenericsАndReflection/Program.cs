f71e2f5a531126fa9207cf879166d1a465847b60
mindball <mincho.balaliew@gmail.com>
2022-03-18 16:31:35 +0200

    Start tutorial Generic with reflection. Closed constructed types and unbound generic

---
+++ Demo/GenericsАndReflection/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsАndReflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ClosedConstructedTyped 
+            var employeeList = CreateCollectionCCT(typeof(List<Employee>));
+            Console.WriteLine(employeeList);
+
+            var employeeListUnbound = CreateCollectionUBGT(typeof(List<>), typeof(Employee));
+            var genericArguments = employeeListUnbound.GetType().GenericTypeArguments;
+            Console.Write(employeeListUnbound.GetType().Name);
+            foreach (var arg in genericArguments)
+            {
+                Console.Write($"[{arg.Name}]");
+            }
+            ;
+        }
+
+        //ClosedConstructedTyped 
+        private static object CreateCollectionCCT(Type type)
+        {
+            return Activator.CreateInstance(type);
+        }
+
+        //Unbound generic type 
+        private static object CreateCollectionUBGT(Type collectionType, Type itemType)
+        {
+            var closedType = collectionType.MakeGenericType(itemType);
+            return Activator.CreateInstance(closedType);
+        }
+    }
+
+    public class Employee
+    {
+        public string Name { get; set; }
+        
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Demo/GenericsАndReflection/Readme.md
@@ -0,0 +1,23 @@
+﻿## CLS 
+```
+ Common Language Specification, 
+```
+![CLS](CLS.JPG "CLS")
+
+### Closed Constructed Typed" CCT
+
+```c#
+//generic type and generic type arguments, these piece of information is called "Closed Constructed Typed" CCT
+//CCT is something it can instance it 
+
+object.GetType() + object.GetType().GenericTypeArguments;
+```
+![ClosedConstructedTyped](ClosedConstructedTyped.JPG "CCT")
+
+### Unbound generic type
+```
+An unbound generic type is not itself a type, and cannot be used as the type of a variable, argument or return value, 
+or as a base type. The only construct in which an unbound generic type can be referenced is the typeof expression
+Its impossible to istantenced it, throw "Type.ContainsGenericParameters is true"
+```
+![Unbound generic type](unboundGeneric.JPG "Unbound")
